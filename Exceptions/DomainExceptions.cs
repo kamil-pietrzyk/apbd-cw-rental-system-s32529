933b997d4e0dff03c35963a0d941861d19a7485e
Kamil Pietrzyk <s32529@pjwstk.edu.pl>
2026-03-25 22:35:21 +0100

    Exceptions implementation

---
+++ Exceptions/DomainExceptions.cs
@@ -0,0 +1,9 @@
+﻿namespace apbd_cw_rental_system_s32529.Exceptions;
+
+public class DomainException(string message) : Exception(message);
+
+public class UserLimitExceededException(string message) : DomainException(message);
+
+public class EquipmentUnavailableException(string message) : DomainException(message);
+
+public class EquipmentNotFoundException(string message) : DomainException(message);
