b2f253f2b8bea3b033a4a14f62d50e8bdd6d92a9
Kamil Pietrzyk <s32529@pjwstk.edu.pl>
2026-03-25 22:36:00 +0100

    Policies implementation

---
+++ Policies/IPenaltyCalculator.cs
@@ -0,0 +1,18 @@
+﻿namespace apbd_cw_rental_system_s32529.Policies;
+
+public interface IPenaltyCalculator
+{
+    decimal CalculatePenalty(DateTime dueDate, DateTime returnDate);
+}
+
+// Przykładowa implementacja: stała kwota za każdy dzień zwłoki
+public class StandardPenaltyCalculator(decimal dailyRate) : IPenaltyCalculator
+{
+    public decimal CalculatePenalty(DateTime dueDate, DateTime returnDate)
+    {
+        if (returnDate <= dueDate) return 0m;
+        
+        int daysLate = (returnDate - dueDate).Days;
+        return daysLate * dailyRate;
+    }
+}
