11e9f223bed08244294ee5e57d3a9fad72ceb5a3
Kamil Pietrzyk <s32529@pjwstk.edu.pl>
2026-03-25 21:49:34 +0100

    Domain model implementation: Rentals

---
+++ Domain/Rentals/Rental.cs
@@ -0,0 +1,24 @@
+﻿using apbd_cw_rental_system_s32529.Domain.Users;
+using apbd_cw_rental_system_s32529.Domain.Equipment;
+
+namespace apbd_cw_rental_system_s32529.Domain.Rentals;
+
+public class Rental(User borrower, EquipmentItem item, DateTime rentDate, TimeSpan duration)
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public User Borrower { get; } = borrower;
+    public EquipmentItem Item { get; } = item;
+    public DateTime RentDate { get; } = rentDate;
+    public DateTime DueDate { get; } = rentDate.Add(duration);
+    public DateTime? ReturnDate { get; private set; }
+    public decimal PenaltyFee { get; private set; }
+    
+    public bool IsActive => ReturnDate == null;
+    public bool IsOverdue => IsActive && DateTime.Now > DueDate;
+
+    public void CompleteReturn(DateTime returnDate, decimal penaltyFee)
+    {
+        ReturnDate = returnDate;
+        PenaltyFee = penaltyFee;
+    }
+}
